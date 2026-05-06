fc30dd43ea004e3a99d558b97ace199d98b8eab5
Gergely Tokodi <gergely.tokodi@gmail.com>
2026-05-06 11:34:39 +0200

    initial commit

---
+++ PasswordValidatorTests/PasswordValidator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Validator;
+
+namespace Validator.Tests
+{
+    [TestClass]
+    public class PasswordValidatorTests
+    {
+        private PasswordValidator _validator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _validator = new PasswordValidator();
+        }
+
+        [TestMethod]
+        public void ValidPassword_ReturnsTrue()
+        {
+            var result = _validator.IsValid("Test1234");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TooShortPassword_ReturnsFalse()
+        {
+            var result = _validator.IsValid("T1a");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NoUpperCase_ReturnsFalse()
+        {
+            var result = _validator.IsValid("test1234");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NoDigit_ReturnsFalse()
+        {
+            var result = _validator.IsValid("TestTest");
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Validator/PasswordValidator.cs
@@ -0,0 +1,25 @@
+﻿namespace Validator
+{
+    public class PasswordValidator
+    {
+        public bool IsValid(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return false;
+
+            if (password.Length < 8)
+                return false;
+
+            if (!password.Any(char.IsUpper))
+                return false;
+
+            if (!password.Any(char.IsLower))
+                return false;
+
+            if (!password.Any(char.IsDigit))
+                return false;
+
+            return true;
+        }
+    }
+}
